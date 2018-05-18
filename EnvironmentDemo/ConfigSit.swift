1ecca16ace995f2d49bfbc43aee56ca0a4fca19d
neo.alienson <neo@orz.hk>
2018-05-18 18:41:25 +0800

    Setup 2 'Environments'

---
+++ EnvironmentDemo/ConfigSit.swift
@@ -0,0 +1,13 @@
+//
+//  ConfigSit.swift
+//  EnvironmentDemoSIT
+//
+//  Created by Neo on 18/5/2018.
+//  Copyright © 2018 Neo. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    static let Name = "SIT"
+}
---
+++ EnvironmentDemo/ConfigUat.swift
@@ -0,0 +1,13 @@
+//
+//  ConfigUat.swift
+//  EnvironmentDemoUAT
+//
+//  Created by Neo on 18/5/2018.
+//  Copyright © 2018 Neo. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    static let Name = "UAT"
+}
